463d171779526361b3f9cb708587ca11c7501d64
Paul Espinosa <paulespin@gmail.com>
2014-11-23 23:30:38 -0800

    [Finished #83011144]: Basic upload functionality is implemented in FileUploadController.

--- irp-webapp/src/main/java/org/cresst/sb/irp/upload/FileUploadController.java
+++ irp-webapp/src/main/java/org/cresst/sb/irp/upload/FileUploadController.java
@@ -1,12 +1,18 @@
 package org.cresst.sb.irp.upload;
 
 import org.apache.log4j.Logger;
+import org.cresst.sb.irp.domain.tdsreport.TDSReport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.XmlMappingException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 
 /**
@@ -14,21 +20,33 @@ import java.io.IOException;
  */
 @Controller
 public class FileUploadController {
-
     private static Logger logger = Logger.getLogger(FileUploadController.class);
 
+    @Autowired
+    private Unmarshaller unmarshaller;
+
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public String upload(@RequestParam("file") MultipartFile file) {
+    public ModelAndView upload(@RequestParam("file") MultipartFile file) {
+
+        String validationMessage = "Invalid";
+
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("report");
 
         if (!file.isEmpty()) {
             try {
-                byte[] bytes = file.getBytes();
-
-            } catch (IOException e) {
+                TDSReport tdsReport = (TDSReport)unmarshaller.unmarshal(new StreamSource(file.getInputStream()));
+                // TODO: Run validation engine on TDS Report and return an analysis report
+                if (tdsReport != null) {
+                    validationMessage = "Valid";
+                }
+            } catch (IOException | XmlMappingException | ClassCastException e) {
                 logger.info("File upload failed", e);
             }
         }
 
-        return "report";
+        mav.addObject("validation", validationMessage);
+
+        return mav;
     }
 }
