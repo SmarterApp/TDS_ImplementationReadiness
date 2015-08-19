03a7f70e64de84dfa55507ccc82eca0107e39c67
Paul Espinosa <paulespin@gmail.com>
2015-08-18 23:13:32 -0700

    Added version number. Added vendor name input box.

--- irp-analysis-engine/src/main/java/org/cresst/sb/irp/domain/analysis/AnalysisResponse.java
+++ irp-analysis-engine/src/main/java/org/cresst/sb/irp/domain/analysis/AnalysisResponse.java
@@ -1,6 +1,7 @@
 package org.cresst.sb.irp.domain.analysis;
 
 import com.google.common.collect.ImmutableList;
+import org.joda.time.DateTime;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,9 @@ import java.util.Objects;
 public class AnalysisResponse {
     // Each IndividualResponse represents each TDSReport xml file and it includes all analyze results
     private List<IndividualResponse> individualResponses = new ArrayList<>();
+    private String vendorName;
+    private String irpVersion;
+    private String dateTimeAnalyzed;
 
     public ImmutableList<IndividualResponse> getIndividualResponses() {
         return ImmutableList.copyOf(individualResponses);
@@ -18,16 +22,43 @@ public class AnalysisResponse {
         this.individualResponses.add(individualResponse);
     }
 
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getIrpVersion() {
+        return irpVersion;
+    }
+
+    public void setIrpVersion(String irpVersion) {
+        this.irpVersion = irpVersion;
+    }
+
+    public String getDateTimeAnalyzed() {
+        return dateTimeAnalyzed;
+    }
+
+    public void setDateTimeAnalyzed(String dateTimeAnalyzed) {
+        this.dateTimeAnalyzed = dateTimeAnalyzed;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         AnalysisResponse that = (AnalysisResponse) o;
-        return Objects.equals(individualResponses, that.individualResponses);
+        return Objects.equals(individualResponses, that.individualResponses) &&
+                Objects.equals(vendorName, that.vendorName) &&
+                Objects.equals(irpVersion, that.irpVersion) &&
+                Objects.equals(dateTimeAnalyzed, that.dateTimeAnalyzed);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(individualResponses);
+        return Objects.hash(individualResponses, vendorName, irpVersion, dateTimeAnalyzed);
     }
 }
