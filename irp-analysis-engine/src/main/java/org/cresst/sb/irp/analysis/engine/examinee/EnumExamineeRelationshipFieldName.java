ba9ab193f648a8ef6cd6f4ff96e44c4752c40100
Paul Espinosa <paulespin@gmail.com>
2016-05-08 23:34:27 +0700

    Updated EmployeeRelationship fields to match those found in the Test Results Transmission Format documentation.

--- irp-analysis-engine/src/main/java/org/cresst/sb/irp/analysis/engine/examinee/EnumExamineeRelationshipFieldName.java
+++ irp-analysis-engine/src/main/java/org/cresst/sb/irp/analysis/engine/examinee/EnumExamineeRelationshipFieldName.java
@@ -2,14 +2,35 @@ package org.cresst.sb.irp.analysis.engine.examinee;
 
 /**
  * Enum for the Examinee Relationships
- * This is the list found in the project tdsdll_release file ReportingDLL.java
+ * The order matches the way they are defined in the TRT document.
  */
 public enum EnumExamineeRelationshipFieldName {
-    ResponsibleDistrictIdentifier,
-    OrganizationName,
-    ResponsibleInstitutionIdentifier,
-    NameOfInstitution,
-    StateName,
-    StateAbbreviation,
-    contextDate
+    StateAbbreviation(2),
+    DistrictId(40, false),
+    DistrictName(60, false),
+    SchoolId(40, false),
+    SchoolName(60, false),
+    StateName(50, false),
+    StudentGroupName(50, false);
+
+    private int maxWidth;
+    private boolean isRequired;
+
+    EnumExamineeRelationshipFieldName(int maxWidth) {
+        this.maxWidth = maxWidth;
+        this.isRequired = true;
+    }
+
+    EnumExamineeRelationshipFieldName(int maxWidth, boolean isRequired) {
+        this.maxWidth = maxWidth;
+        this.isRequired = isRequired;
+    }
+
+    public int getMaxWidth() {
+        return maxWidth;
+    }
+
+    public boolean isRequired() {
+        return isRequired;
+    }
 }
