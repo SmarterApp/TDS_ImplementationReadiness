02dbcc89d5494e0386463d7899432024a928bb73
Stefan Eng <stefaneng@g.ucla.edu>
2016-10-11 15:18:11 -0700

    IRP-190: validate item data csv with apache CSVParser

--- irp-service/src/main/java/org/cresst/sb/irp/service/CATAnalysisServiceImpl.java
+++ irp-service/src/main/java/org/cresst/sb/irp/service/CATAnalysisServiceImpl.java
@@ -1,5 +1,11 @@
 package org.cresst.sb.irp.service;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -12,7 +18,18 @@ public class CATAnalysisServiceImpl implements CATAnalysisService {
     @Override
     public boolean validateItemCsv(MultipartFile itemFile) {
         logger.info("Validating CSV definition for: " + itemFile.getName());
-        return false;
-    }
 
+        Set<String> expectedHeaders = new HashSet<>();
+        expectedHeaders.add("SID");
+        expectedHeaders.add("ItemID");
+        expectedHeaders.add("Score");
+
+        try {
+            CSVParser parser = CSVParser.parse(new String(itemFile.getBytes()), CSVFormat.EXCEL.withHeader());
+            Set<String> headers = parser.getHeaderMap().keySet();
+            return headers.containsAll(expectedHeaders);
+        } catch (IOException e) {
+            return false;
+        }
+    }
 }
