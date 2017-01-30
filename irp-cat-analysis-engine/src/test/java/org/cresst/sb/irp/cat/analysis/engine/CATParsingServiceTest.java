a73ca1259a26e6e38f01704282c746242299e250
Stefan Eng <stefaneng@ucla.edu>
2017-01-30 15:27:55 -0800

    Make Unit tests for parsing student scores

--- irp-cat-analysis-engine/src/test/java/org/cresst/sb/irp/cat/analysis/engine/CATParsingServiceTest.java
+++ irp-cat-analysis-engine/src/test/java/org/cresst/sb/irp/cat/analysis/engine/CATParsingServiceTest.java
@@ -10,6 +10,9 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import org.cresst.sb.irp.cat.domain.analysis.BlueprintStatement;
+import org.cresst.sb.irp.cat.domain.analysis.ELAStudentScoreCAT;
+import org.cresst.sb.irp.cat.domain.analysis.ItemResponseCAT;
+import org.cresst.sb.irp.cat.domain.analysis.MathStudentScoreCAT;
 import org.cresst.sb.irp.cat.domain.analysis.PoolItemELA;
 import org.cresst.sb.irp.cat.domain.analysis.TrueTheta;
 import org.cresst.sb.irp.cat.domain.analysis.ViolationCount;
@@ -74,4 +77,60 @@ public class CATParsingServiceTest {
         assertEquals("Claim 1 , test2, DOK = 1 , Targets: 1, 3", row2.getSpecification());
         assertEquals("NA", row2.getMaxStr());
     }
+
+    @Test
+    public void testParseItemCsv() throws IOException {
+        final String itemString = "sId, itemId, score\n1,1,1";
+        final InputStream itemStream = stringToInputStream(itemString);
+        List<ItemResponseCAT> items = service.parseItemCsv(itemStream);
+        assertEquals(1, items.size());
+
+        ItemResponseCAT item = items.get(0);
+        assertEquals("1", item.getItemId());
+        assertEquals("1", item.getsId());
+        assertEquals(1, item.getScore());
+    }
+
+    @Test
+    public void testParseStudentScoreELACAT() throws IOException {
+        final String itemString = "SID,Overall,Overall_SEM,Claim1,Claim1_SEM,Claim2,Claim2_SEM,Claim3,Claim3_SEM,Claim4,Claim4_SEM\n"
+                + "1, 5.0, 0.5, 1.0, 0.1, 2.0, 0.2, 3.0, 0.3, 4.0, 0.4";
+        final InputStream itemStream = stringToInputStream(itemString);
+        List<ELAStudentScoreCAT> items = service.parseStudentELACsv(itemStream);
+        assertEquals(1, items.size());
+
+        ELAStudentScoreCAT item = items.get(0);
+        assertEquals("1", item.getSid());
+        assertEquals(5.0, item.getOverallScore(), DELTA);
+        assertEquals(0.5, item.getOverallSEM(), DELTA);
+        assertEquals(1.0, item.getClaim1Score(), DELTA);
+        assertEquals(0.1, item.getClaim1SEM(), DELTA);
+        assertEquals(2.0, item.getClaim2Score(), DELTA);
+        assertEquals(0.2, item.getClaim2SEM(), DELTA);
+        assertEquals(3.0, item.getClaim3Score(), DELTA);
+        assertEquals(0.3, item.getClaim3SEM(), DELTA);
+        assertEquals(4.0, item.getClaim4Score(), DELTA);
+        assertEquals(0.4, item.getClaim4SEM(), DELTA);
+    }
+
+    @Test
+    public void testParseStudentScoreMath() throws IOException {
+        final String itemString = "SID,Overall,Overall_SEM,Claim1,Claim1_SEM,Claim2_4Score,Claim2_4SEM,Claim3,Claim3_SEM\n"
+                + "1, 5.0, 0.5, 1.0, 0.1, 2.4, 4.2, 3.0, 0.3";
+        final InputStream itemStream = stringToInputStream(itemString);
+        List<MathStudentScoreCAT> items = service.parseStudentMathCsv(itemStream);
+        assertEquals(1, items.size());
+
+        MathStudentScoreCAT item = items.get(0);
+        assertEquals("1", item.getSid());
+        assertEquals(5.0, item.getOverallScore(), DELTA);
+        assertEquals(0.5, item.getOverallSEM(), DELTA);
+        assertEquals(1.0, item.getClaim1Score(), DELTA);
+        assertEquals(0.1, item.getClaim1SEM(), DELTA);
+        assertEquals(2.4, item.getClaim2_4Score(), DELTA);
+        assertEquals(4.2, item.getClaim2_4SEM(), DELTA);
+        assertEquals(3.0, item.getClaim3Score(), DELTA);
+        assertEquals(0.3, item.getClaim3SEM(), DELTA);
+    }
+
 }
