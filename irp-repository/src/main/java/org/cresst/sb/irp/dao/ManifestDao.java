5ef24ce25c03927bdf8d234ad85af9faee33e72f
Stefan Eng <stefaneng@ucla.edu>
2017-02-15 13:45:11 -0800

    Fix docs for Manifest DAO

--- irp-repository/src/main/java/org/cresst/sb/irp/dao/ManifestDao.java
+++ irp-repository/src/main/java/org/cresst/sb/irp/dao/ManifestDao.java
@@ -5,7 +5,7 @@ import org.cresst.sb.irp.domain.manifest.Manifest;
 import java.util.List;
 
 /**
- * Data repository for Manifest data. Enables CRUD operations on the manifest
+ * Data repository for Manifest data. Enables query operations on the manifest
  * from the irp-package.
  */
 public interface ManifestDao {
