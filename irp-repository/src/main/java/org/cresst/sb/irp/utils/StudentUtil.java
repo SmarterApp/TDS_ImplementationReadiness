14ec1238b5806d0bdddb17a2d6317c31c933a29a
Paul Espinosa <paulespin@gmail.com>
2017-03-01 00:30:41 +0900

    Update student file processing utility
    The StudentUtil class was using ‘AlternateSSID’ which is the old
    name for ‘ExternalSSID’ found in the Student file.

--- irp-repository/src/main/java/org/cresst/sb/irp/utils/StudentUtil.java
+++ irp-repository/src/main/java/org/cresst/sb/irp/utils/StudentUtil.java
@@ -1,13 +1,7 @@
 package org.cresst.sb.irp.utils;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.cresst.sb.irp.domain.student.Student;
@@ -15,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
 
 /**
  * Student utilities
@@ -43,19 +39,14 @@ public class StudentUtil {
 			Map<Integer, String> headerMap, XSSFSheet sheet,
 			ExcelUtil excelUtil, 
 			int startIndex) {
-		try {
-			for (int rowCount = startIndex; rowCount <= sheet.getLastRowNum(); rowCount++) {
-				Row detailRow = sheet.getRow(rowCount);
-				if (detailRow != null && !excelUtil.isEmptyRow(detailRow)) {
-					Student student = createObject(detailRow, headerMap, excelUtil);
-					if (student != null)
-						listStudent.add(student);
-				}
-			}
-		} catch (Exception e) {
-			logger.error("processSheet(); exception: ", e);
-		}
 
+		for (int rowCount = startIndex; rowCount <= sheet.getLastRowNum(); rowCount++) {
+			Row detailRow = sheet.getRow(rowCount);
+			if (detailRow != null && !excelUtil.isEmptyRow(detailRow)) {
+				Student student = createObject(detailRow, headerMap, excelUtil);
+                listStudent.add(student);
+			}
+		}
 	}
 
     /**
@@ -69,26 +60,19 @@ public class StudentUtil {
      * @return student object represented by `row`
      */
 	private Student createObject(Row row, Map<Integer, String> headerMap, ExcelUtil excelUtil) {
-		Student student = null;
-		try {
-			student = new Student();
-			int headerSize = headerMap.size();
-			for (int cellCount = 0; cellCount < headerSize; cellCount++) {
-				Cell cell = row.getCell(cellCount);
-				String columnName = headerMap.get(cellCount);
-				if (cell != null) {
-					if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
-						cell.setCellType(Cell.CELL_TYPE_STRING);
-					} else if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
-					} else if (cell.getCellType() == Cell.CELL_TYPE_BLANK) {
-					} 
-					
-					setFieldData(student, columnName, cell.getStringCellValue(), excelUtil);
-				}
-			}
 
-		} catch (Exception e) {
-			logger.error("createObject(); exception: ", e);
+		Student student = new Student();
+		int headerSize = headerMap.size();
+		for (int cellCount = 0; cellCount < headerSize; cellCount++) {
+			Cell cell = row.getCell(cellCount);
+			String columnName = headerMap.get(cellCount);
+			if (cell != null) {
+				if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+					cell.setCellType(Cell.CELL_TYPE_STRING);
+				}
+
+				setFieldData(student, columnName, cell.getStringCellValue(), excelUtil);
+			}
 		}
 
 		return student;
@@ -107,37 +91,36 @@ public class StudentUtil {
      *            value to set in `student`
      */
 	private void setFieldData(Student student, String columnName, String cellStringValue, ExcelUtil excelUtil) {
-		try {
-			columnName = StringUtils.deleteWhitespace(columnName);
-			switch (columnName.toLowerCase()) {
-			case "stateabbreviation": 
-				student.setStateAbbreviation(cellStringValue);
-				break;
-			case "responsibledistrictidentifier":
-				student.setResponsibleDistrictIdentifier(cellStringValue);
-				break;
-			case "responsibleinstitutionidentifier": //"ResponsibleSchoolIdentifier":
-				student.setResponsibleInstitutionIdentifier(cellStringValue);
-				break;
-			case "lastorsurname":
-				student.setLastOrSurname(cellStringValue);
-				break;
-			case "firstname":
-				student.setFirstName(cellStringValue);
-				break;
-			case "middlename":
-				student.setMiddleName(cellStringValue);
-				break;
-			case "birthdate":
-				student.setBirthdate(excelUtil.dateString(cellStringValue));
-				break;
-			case "ssid":
-				student.setSSID(cellStringValue);
-				break;	
-			case "alternatessid":
-				student.setAlternateSSID(cellStringValue);
-				break;		
-			/*	
+        columnName = StringUtils.deleteWhitespace(columnName);
+        switch (columnName.toLowerCase()) {
+            case "stateabbreviation":
+                student.setStateAbbreviation(cellStringValue);
+                break;
+            case "responsibledistrictidentifier":
+                student.setResponsibleDistrictIdentifier(cellStringValue);
+                break;
+            case "responsibleinstitutionidentifier": //"ResponsibleSchoolIdentifier":
+                student.setResponsibleInstitutionIdentifier(cellStringValue);
+                break;
+            case "lastorsurname":
+                student.setLastOrSurname(cellStringValue);
+                break;
+            case "firstname":
+                student.setFirstName(cellStringValue);
+                break;
+            case "middlename":
+                student.setMiddleName(cellStringValue);
+                break;
+            case "birthdate":
+                student.setBirthdate(excelUtil.dateString(cellStringValue));
+                break;
+            case "ssid":
+                student.setSSID(cellStringValue);
+                break;
+            case "externalssid":
+                student.setAlternateSSID(cellStringValue);
+                break;
+			/*
 			case "StudentIdentifier":
 				student.setStudentIdentifier(cellStringValue);
 				break;
@@ -148,79 +131,76 @@ public class StudentUtil {
 				student.setConfirmationCode(cellStringValue);
 				break;
 			*/
-			case "gradelevelwhenassessed":
-				student.setGradeLevelWhenAssessed(StringUtils.leftPad(cellStringValue, 2, '0'));
-				break;
-			case "sex":
-				student.setSex(cellStringValue);
-				break;
-			case "hispanicorlatinoethnicity":
-				student.setHispanicOrLatinoEthnicity(cellStringValue);
-				break;
-			case "americanindianoralaskanative":
-				student.setAmericanIndianOrAlaskaNative(cellStringValue);
-				break;
-			case "asian":
-				student.setAsian(cellStringValue);
-				break;
-			case "blackorafricanamerican":
-				student.setBlackOrAfricanAmerican(cellStringValue);
-				break;
-			case "white":
-				student.setWhite(cellStringValue);
-				break;
-			case "nativehawaiianorotherpacificislander":
-				student.setNativeHawaiianOrOtherPacificIslander(cellStringValue);
-				break;
-			case "demographicracetwoormoreraces":
-				student.setDemographicRaceTwoOrMoreRaces(cellStringValue);
-				break;
-			case "ideaindicator":
-				student.setIDEAIndicator(cellStringValue);
-				break;
-			case "lepstatus":
-				student.setLEPStatus(cellStringValue);
-				break;
-			case "section504status":
-				student.setSection504Status(cellStringValue);
-				break;
-			case "economicdisadvantagestatus":
-				student.setEconomicDisadvantageStatus(cellStringValue);
-				break;
-			case "languagecode":
-				student.setLanguageCode(cellStringValue);
-				break;
-			case "englishlanguageproficiencylevel":
-				student.setEnglishLanguageProficiencyLevel(cellStringValue);
-				break;
-			case "migrantstatus":
-				student.setMigrantStatus(cellStringValue);
-				break;
-			case "firstentrydateintousschool":
-				student.setFirstEntryDateIntoUSSchool(excelUtil.dateString(cellStringValue));
-				break;
-			case "limitedenglishproficiencyentrydate":
-				student.setLimitedEnglishProficiencyEntryDate(cellStringValue);
-				break;
-			case "lepexitdate":
-				student.setLEPExitDate(cellStringValue);
-				break;
-			case "titleiiilanguageinstructionprogramtype":
-				student.setTitleIIILanguageInstructionProgramType(cellStringValue);
-				break;
-			case "primarydisabilitytype":
-				student.setPrimaryDisabilityType(cellStringValue);
-				break;
-			case "delete":
-				student.setDelete(cellStringValue);
-				break;
-			default:
-				throw new IllegalArgumentException("Invalid column name: "
-						+ columnName);
-			}
-		} catch (Exception e) {
-			logger.error("setFieldData(); exception: ", e);
-		}
+            case "gradelevelwhenassessed":
+                student.setGradeLevelWhenAssessed(StringUtils.leftPad(cellStringValue, 2, '0'));
+                break;
+            case "sex":
+                student.setSex(cellStringValue);
+                break;
+            case "hispanicorlatinoethnicity":
+                student.setHispanicOrLatinoEthnicity(cellStringValue);
+                break;
+            case "americanindianoralaskanative":
+                student.setAmericanIndianOrAlaskaNative(cellStringValue);
+                break;
+            case "asian":
+                student.setAsian(cellStringValue);
+                break;
+            case "blackorafricanamerican":
+                student.setBlackOrAfricanAmerican(cellStringValue);
+                break;
+            case "white":
+                student.setWhite(cellStringValue);
+                break;
+            case "nativehawaiianorotherpacificislander":
+                student.setNativeHawaiianOrOtherPacificIslander(cellStringValue);
+                break;
+            case "demographicracetwoormoreraces":
+                student.setDemographicRaceTwoOrMoreRaces(cellStringValue);
+                break;
+            case "ideaindicator":
+                student.setIDEAIndicator(cellStringValue);
+                break;
+            case "lepstatus":
+                student.setLEPStatus(cellStringValue);
+                break;
+            case "section504status":
+                student.setSection504Status(cellStringValue);
+                break;
+            case "economicdisadvantagestatus":
+                student.setEconomicDisadvantageStatus(cellStringValue);
+                break;
+            case "languagecode":
+                student.setLanguageCode(cellStringValue);
+                break;
+            case "englishlanguageproficiencylevel":
+                student.setEnglishLanguageProficiencyLevel(cellStringValue);
+                break;
+            case "migrantstatus":
+                student.setMigrantStatus(cellStringValue);
+                break;
+            case "firstentrydateintousschool":
+                student.setFirstEntryDateIntoUSSchool(excelUtil.dateString(cellStringValue));
+                break;
+            case "limitedenglishproficiencyentrydate":
+                student.setLimitedEnglishProficiencyEntryDate(cellStringValue);
+                break;
+            case "lepexitdate":
+                student.setLEPExitDate(cellStringValue);
+                break;
+            case "titleiiilanguageinstructionprogramtype":
+                student.setTitleIIILanguageInstructionProgramType(cellStringValue);
+                break;
+            case "primarydisabilitytype":
+                student.setPrimaryDisabilityType(cellStringValue);
+                break;
+            case "delete":
+                student.setDelete(cellStringValue);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid column name: "
+                        + columnName);
+        }
 	}
 
 }
